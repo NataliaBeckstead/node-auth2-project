0caf4b924cd2d33e9e3511d5debdd344d230756f
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-21 17:06:57 -0600

    stretch attempt

--- users/users-model.js
+++ users/users-model.js
@@ -1,4 +1,6 @@
 const db = require("../database/dbConfig.js");
+const jwt = require('jsonwebtoken');
+const secrets = require('../auth/secrets.js');
 
 module.exports = {
   add,
@@ -7,13 +9,33 @@ module.exports = {
   findById,
 };
 
-function find() {
-  return db("users").select("id", "username", "department");
+// function find() {
+//   return db("users").select("id", "username", "department");
+// }
+
+// function find(token) {
+//   var grab;
+//   jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+//     grab = decodedToken;
+//   })
+//   console.log(grab);
+//   return db("users")
+//   .select("id", "username","department")
+//   .where("department", grab.department)
+// }
+
+function find(filter) {
+  if (filter) return db('users').where(filter);
+  else return db('users');
 }
 
 function findBy(filter) {
   return db("users").where(filter);
 }
+
+// function findBy(filter) {
+//   return db("users").where(filter).select("id", "username", "department");
+// }
 
 async function add(user) {
   const [id] = await db("users").insert(user, "id");
--- users/users-router.js
+++ users/users-router.js
@@ -2,12 +2,35 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-});
+// router.get("/", (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
+
+// router.get("/", (req, res) => {
+//   Users.find(req.headers.authorization)
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
+
+router.get('/', (req, res) => {
+  const { department } = req.decoded;
+  Users.find({ department }).then(users => res.status(200).json(users))
+  .catch(error => res.status(500).json({ error: error.message }))
+})
+
+// router.get(`/:department`, (req, res) => {
+//   const { department }= req.params;
+//   Users.findBy({department})
+//   .then(users => {
+//       res.json(users);
+//   })
+//   .catch(err => res.send(err));
+// })
 
 module.exports = router;
